965c52ab70c2a9a94577431b2d1b6f4e35018df2
Cosette37 <69123930+Cosette37@users.noreply.github.com>
2020-12-19 22:29:35 -0600

    UFO 1 Complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,64 +2,35 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// select the filter button
+// select the filter button/form/tablebody
 var button = d3.select("#filter-btn");
-var form = d3.select("#datetime");
-var tbody = d3.select("tbody")
-//select the form
-
-data.forEach((tableData) => {
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-//Create event handlers
-
+var form = d3.select("#form-id");
+var tbody = d3.select("tbody");
+ //Create event handlers
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
 //Complete event handler
-
 function runEnter() {
     //prevent page from refreshing
-    d3.event.preventDefault();
+  d3.event.preventDefault();
     //select input from datetime entry
-    var inputElement = d3.select("#datetime");
+  var inputElement = d3.select("#datetime");
     //get the value of the input
-    var inputValue = inputElement.property("value");
-    
+  var inputValue = inputElement.property("value");
     //filter data
-    var filteredData = tableData.filter(item => item.datetime === inputValue); 
-    //console.log(tableData);
-    // console.log(data);
-    console.log(filteredData);
-}
- data.forEach((filteredData) => {
-    var row =tbody.filter("tr");
-    Object.entries(filteredData).forEach(([key,value]) => {
-        var cell =row.filter("td");
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue); 
+  //clear table for next filter
+  tbody.html("");
+  //console.log(filteredData);
+//put filtered data into the table  
+  filteredData.forEach((rowData) => { 
+   
+    let row = tbody.append("tr");
+      Object.values(rowData).forEach((value) => {
+        let cell = row.append("td");
         cell.text(value);
-    });
-    
-    //Select the table
-    //var table = d3.select(tableData);
-    //console.log(table);
-    //create a striped table with bootstrap
-
-    //table.attr("class", "table table-striped");
-    //http://getbootstrap.com/docs/3.3/css/#tables-striped
-    //select table body
-    //var tbody=d3.select("tbody");
-    //append table row to the table body
-    //var row =tbody.append("tr");
-    //append one cell to row for each datapoint
-    //row.append("td").text([0].datetime);
-    //row.append("td").text([0].city);
-    //row.append("td").text(tableData.state);
-    //row.append("td").text(tableData.country);
-    //row.append("td").text(tableData.shape);
-    //row.append("td").text(tableData.durationMinutes);
-    //row.append("td").text(tableData.comments);
-});
+          });
+        })
+        
+};
